b470c79fef415de198e8cf8a7ac7c9d4460c7d03
Woojin <jwj3171@gmail.com>
2025-08-01 13:12:39 +0900

    아무것도 안한 임시 커밋

---
+++ app/login/page.js
@@ -0,0 +1,11 @@
+// app/login/page.js
+
+export default function LoginPage() {
+  return (
+    // @TODO CSS
+    <div style={{ padding: "2rem" }}>
+      {/* <LoginForm /> */}
+      로그인 페이지입니다
+    </div>
+  );
+}
