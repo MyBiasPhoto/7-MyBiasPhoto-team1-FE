89775f7f3fd559cfa7588114df807ea14b344e45
dudska12 <skadud0113@naver.com>
2025-08-08 15:31:52 +0900

    판매글 내리기 모달 연동 및 백엔드연동 #14

--- components/modals/sellCancelModal.js
+++ components/modals/sellCancelModal.js
@@ -3,8 +3,28 @@
 import styles from "./sellCancelModal.module.css";
 import Image from "next/image";
 import CloseIcon from "@/public/icons/ic_close.svg";
+import { useCancelSale } from "../marketPlace/hook/useCancleSale/useCancleSale";
+import { useRouter } from "next/navigation";
 
-const SellCancelModal = ({ onClose }) => {
+const SellCancelModal = ({ onClose, saleId }) => {
+  const router = useRouter();
+  const { mutate, isPending, isError } = useCancelSale();
+
+  const handleCancelSale = () => {
+    mutate(
+      { id: saleId },
+      {
+        onSuccess: () => {
+          onClose();
+          router.push("/marketPlace");
+        },
+        onError: (error) => {
+          console.error("판매 중단 실패:", error);
+          alert("판매 중단에 실패했습니다.");
+        },
+      }
+    );
+  };
   return (
     <div className={styles.overlay}>
       <div className={styles.modal}>
@@ -13,7 +33,7 @@ const SellCancelModal = ({ onClose }) => {
         </button>
         <h2 className={styles.title}>포토카드 판매 내리기</h2>
         <p className={styles.description}>정말로 판매를 중단하시겠습니까?</p>
-        <button className={styles.confirmButton} onClick={onClose}>
+        <button onClick={handleCancelSale} className={styles.confirmButton}>
           판매 내리기
         </button>
       </div>
