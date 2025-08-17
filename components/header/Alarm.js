98f833e7364f2a94229a9fed3163259029950247
Woojin <jwj3171@gmail.com>
2025-08-17 21:20:38 +0900

    fix(Alarm): 알림 클릭시 type별 랜딩페이지 지정 #22

--- components/header/Alarm.js
+++ components/header/Alarm.js
@@ -2,6 +2,7 @@
 "use client";
 import styles from "./Alarm.module.css";
 import { useEffect, useRef, useCallback } from "react";
+import { useRouter } from "next/navigation";
 import { useNotifications } from "@/utils/notifications/notificationsContext";
 
 export default function Alarm() {
@@ -13,6 +14,7 @@ export default function Alarm() {
     markAllAsRead,
   } = useNotifications();
 
+  const router = useRouter();
   const sentinelRef = useRef(null);
   //sentinel 보이면 loadMore호출
 
@@ -34,6 +36,20 @@ export default function Alarm() {
     observer.observe(sentinelRef.current);
     return () => observer.disconnect();
   }, [handleIntersect]);
+
+  const handleClickNotification = async (notification) => {
+    try {
+      await markOneAsRead(notification.id);
+    } finally {
+      const fallbackRoute =
+        notification.type === "CARD_PURCHASED" ||
+        notification.type === "CARD_SOLD_OUT"
+          ? "/myGallery" // 이 fallback은 혹시 link가 비어있을 때만 사용됨
+          : "/";
+      const destination = notification.link || fallbackRoute;
+      router.push(destination);
+    }
+  };
 
   return (
     <div className={styles.area}>
@@ -58,12 +74,14 @@ export default function Alarm() {
             className={`${styles.list} ${
               notification.read ? "" : styles.unread
             }`}
-            onClick={() => markOneAsRead(notification.id)}
+            // onClick={() => markOneAsRead(notification.id)}
+            onClick={() => handleClickNotification(notification)}
             type="button"
           >
             <span className={styles.text}>{notification.content}</span>
             <span className={styles.time}>
               {new Date(notification.createdAt).toLocaleString()}
+              {/* @TODO 시간표시 ~시간전 코드잇요구사항대로 */}
             </span>
           </button>
         ))}
