e834d0f423943e22cebd5db0d4d530b4c32331a8
dudska12 <skadud0113@naver.com>
2025-08-19 09:28:22 +0900

    스켈레톤UI 수정중(마켓플레이스)

--- components/marketPlace/skeleton/MarketPlaceCardSkeleton.js
+++ components/marketPlace/skeleton/MarketPlaceCardSkeleton.js
@@ -5,15 +5,20 @@ export default function MarketPlaceCardSkeleton({ count = 6 }) {
   return (
     <div className={style.cardList}>
       {Array.from({ length: count }).map((_, idx) => (
-        <div key={idx} className={style.cardItem}>
-          <div className={style.image}></div>
-          <div className={style.info}></div>
-          <div className={style.title}></div>
-          <div className={style.line}></div>
-          <div className={style.line}></div>
-          <div className={style.line}></div>
-          <div className={style.info}></div>
-          <div className={style.info}></div>
+        <div key={idx} className={style.cardContainer}>
+          <div className={style.skeletonWrapper}>
+            <div className={`${style.skeletonBox} ${style.large}`}></div>
+            <div className={`${style.skeletonBox} ${style.medium}`}></div>
+            <div className={style.subTitle}>
+              <div className={style.subTitleBox}>
+                <div className={`${style.skeletonBox} ${style.small}`}></div>
+                <div className={`${style.skeletonBox} ${style.small}`}></div>
+              </div>
+              <div className={`${style.skeletonBox} ${style.small}`}></div>
+            </div>
+            <div className={`${style.skeletonBox} ${style.content}`}></div>
+            <div className={`${style.skeletonBox} ${style.content}`}></div>
+          </div>
         </div>
       ))}
     </div>
